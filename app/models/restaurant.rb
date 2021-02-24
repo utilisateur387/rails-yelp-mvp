abe0a1e45a0518a981d5d5ff19228d0cedc5dd83
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-02-24 14:25:26 +0100

    Add validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY, message: 'Category is not valid' }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ApplicationRecord
+  RATING = [0, 1, 2, 3, 4, 5]
+  belongs_to :restaurant
+
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: RATING, message: 'Rating is not valid' }
+  validates :content, presence: true
 end
