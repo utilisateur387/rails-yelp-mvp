d1232aed001f47d9faa043e858c281a20d5f11b8
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-02-24 23:50:39 +0100

    edit restaurant controller create redirection

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -14,7 +14,7 @@ class RestaurantsController < ApplicationController
   def create
     @restaurant = Restaurant.new(restaurant_params)
     if @restaurant.save
-      redirect_to restaurants_path, notice: "Restaurant was successfully created"
+      redirect_to restaurant_path(@restaurant), notice: "Restaurant was successfully created"
     else
       render :new
     end
